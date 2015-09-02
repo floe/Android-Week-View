d99c68789b0e6e2bc953a975df9f41ec92f33dc3
Florian Echtler <floe@butterbrot.org>
2015-09-02 20:12:51 +0200

    use non-US date format

--- sample/src/main/java/com/alamkanak/weekview/sample/MainActivity.java
+++ sample/src/main/java/com/alamkanak/weekview/sample/MainActivity.java
@@ -393,7 +393,7 @@ public class MainActivity extends ActionBarActivity implements WeekView.MonthCha
             public String interpretDate(Calendar date) {
                 SimpleDateFormat weekdayNameFormat = new SimpleDateFormat("EEE", Locale.getDefault());
                 String weekday = weekdayNameFormat.format(date.getTime());
-                SimpleDateFormat format = new SimpleDateFormat(" M/d", Locale.getDefault());
+                SimpleDateFormat format = new SimpleDateFormat(" d.M.", Locale.getDefault());
 
                 // All android api level do not have a standard way of getting the first letter of
                 // the week day name. Hence we get the first char programmatically.
